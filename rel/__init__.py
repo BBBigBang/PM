83316685f0db92d623e781b126f7ef7d4de279bf
BBBigBang <136481981@qq.com>
2018-12-06 11:20:02 +0800

    relation

---
+++ rel/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import print_function
+# *************************************************
+# > File Name: __init__.py
+# > Author: yang 
+# > Mail: yangperasd@163.com 
+# > Created Time: Wed 08 Nov 2017 09:42:49 PM CST
+# *************************************************
+from src import load_model;
+from src.RE_run import run;
+from src.RE_run import toJson;
+from src.RE_run2 import run2;
